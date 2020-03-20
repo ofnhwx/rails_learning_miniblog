226db87465c83aa52e8bda6972c0d769144fe402
Yuta Fujita <ofnhwx@komunan.net>
2020-03-20 21:03:59 +0900

    ユーザーのバリデーションを追加
    - ユーザー名
      - アルファベットのみ
      - スペース禁止
      - 20文字以内
    - プロフィール
      - 200文字以内

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       format: { with: /\A[a-zA-Z]+\z/ },
+                       length: { maximum: 20 }
+  validates :profile, length: { maximum: 200 }
 end
