17409a76b490b6ad052f753c646e27014d0ed64f
Yuta Fujita <ofnhwx@komunan.net>
2020-03-21 17:59:56 +0900

    フォローの状態を保存するモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_120740) do
+ActiveRecord::Schema.define(version: 2020_03_21_085459) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2020_03_20_120740) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
