e292bd7fcacb9a3e3b0264fb8c312235436cf52c
Yuta Fujita <ofnhwx@komunan.net>
2020-03-21 18:02:36 +0900

    フォロー, アンフォローのコントローラーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[show]
   resources :posts, only: %i[index new create]
+  resources :relationships, only: %i[create destroy]
 end
