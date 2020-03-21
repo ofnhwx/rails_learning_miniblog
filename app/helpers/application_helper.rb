ca7bbce2ee923e779ff4f9b709184cfb5c3f86d6
Yuta Fujita <ofnhwx@komunan.net>
2020-03-21 17:39:46 +0900

    devise のメッセージに bootstrap の設定が適用されるように対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def convert_to_bootstrap_alert_type(type)
+    case type
+    when 'notice'
+      'success'
+    when 'alert'
+      'warning'
+    when 'error'
+      'danger'
+    else
+      type
+    end
+  end
 end
