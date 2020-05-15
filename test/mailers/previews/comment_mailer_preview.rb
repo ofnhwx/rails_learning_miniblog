4425c94dccb94453194743edc42646268445ff58
Yuta Fujita <ofnhwx@komunan.net>
2020-05-15 09:24:10 +0900

    メールの内容を少しだけ調整(デザインとかは時間かかりそうなので後で勉強する)

--- test/mailers/previews/comment_mailer_preview.rb
+++ test/mailers/previews/comment_mailer_preview.rb
@@ -4,6 +4,6 @@
 class CommentMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/comment_mailer/notify
   def notify
-    CommentMailer.notify
+    CommentMailer.notify(Comment.first)
   end
 end
