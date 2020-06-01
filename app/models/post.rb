10127cd51172d00e67370012a970ce110d3ce0ae
Yuta Fujita <ofnhwx@komunan.net>
2020-06-02 08:11:16 +0900

    画像の投稿を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,6 @@ class Post < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  mount_uploader :image, ImageUploader
 end
