2b30095ca2b8be55004837a9326ffe89c8b08e79
Yuta Fujita <ofnhwx@komunan.net>
2020-06-02 08:10:47 +0900

    画像用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_105253) do
+ActiveRecord::Schema.define(version: 2020_06_01_230904) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_04_19_105253) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.integer "favorites_count", default: 0, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
