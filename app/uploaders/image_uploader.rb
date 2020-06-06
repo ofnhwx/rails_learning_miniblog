9b662499f832bfc1633f0e07476a469c30b00362
Yuta Fujita <ofnhwx@komunan.net>
2020-06-06 23:24:59 +0900

    画像のリサイズ、サムネイルの表示と開発環境でのアップロード先を調整

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
   storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
+
+  version :thumb do
+    process resize_to_limit: [80, 80]
+  end
 end
